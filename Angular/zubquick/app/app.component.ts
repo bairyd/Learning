5cd8beacfa5a5c5b77374c773e1fb9a277226906
Zubair Davids <bairyd@gmail.com>
2016-10-26 16:28:04 +0200

    making some modifications

--- Angular/zubquick/app/app.component.ts
+++ Angular/zubquick/app/app.component.ts
@@ -21,15 +21,16 @@ const HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" )>
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li> 
         </ul>
-        <!--<div><label>id: </label>{{hero.id}}</div>-->
-        <!--<div>-->
-            <!--<label>name: </label>-->
-            <!--<input [(ngModel)]="hero.name" value="{{hero.name}}" placeholder="name"/>-->
-        <!--</div>-->
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+        </div>
     `,
     styles: [
         `
@@ -86,4 +87,8 @@ const HEROES: Hero[] = [
 export class AppComponent {
     title = 'Tour of heroes';
     heroes = HEROES;
+    selectedHero: Hero;
+    onSelect(hero:Hero):void{
+        this.selectedHero = hero;
+    }
 }
